7ea2c6c0b4ff48c9477bc9cd856f3c49fcfacb9b
Guillaume BELDILMI <gbeldilmi@pm.me>
2022-11-25 03:48:50 +0100

    Update run.c

--- game/src/run.c
+++ game/src/run.c
@@ -1,5 +1,55 @@
 #include "game.h"
 
+static int *chosen_cards;
+
+static int win(){
+  int i;
+  for(i = 0; i < num_players; i++){
+    if(players[i]->score >= MAX_SCORE){
+      return 1;
+    }
+  }
+  return 0;
+}
+
+static void play_thread(void *arg){
+  int player;
+  player = (int)arg;
+  play(player, &chosen_cards[player]);
+}
+
 void run(){
-  //
+  pthread_t *threads;
+  int i;
+  threads = (pthread_t *)malloc(num_players * sizeof(pthread_t));
+  chosen_cards = (int *)malloc(num_players * sizeof(int));
+  while(!win()){
+    if(players[0]->stack->size == 0){
+      deal();
+    }
+    for(i = 0; i < num_players; i++){
+      pthread_create(&threads[i], NULL, (void *)play_thread, (void *)i);
+    }
+    for(i = 0; i < num_players; i++){
+      pthread_join(threads[i], NULL);
+    }
+    
+    // play round
+
+    // print cards state
+    
+    // ask cards to players
+
+    // print cards state
+
+    // play cards
+
+    // update scores
+
+    // print cards state
+
+
+  }
+  free(threads);
+  free(chosen_cards);
 }
